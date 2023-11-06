c6c9d42b3ad11e4ad3b0b5fbe9473fdbc6f97a38
David Hunt <david.hunt@duke.edu>
2023-11-05 21:40:20 -0500

    added radcloud config and dataset to mmseg

---
+++ mmseg/datasets/radcloud.py
@@ -0,0 +1,34 @@
+# Xiao 2023-10-04
+from mmseg.registry import DATASETS
+from .basesegdataset import BaseSegDataset
+
+@DATASETS.register_module()
+class RadcloudDataset(BaseSegDataset):
+    """RadcloudDataset dataset.
+
+    In segmentation map annotation for STARE, 0 stands for background, which is
+    included in 2 categories. ``reduce_zero_label`` is fixed to False. The
+    ``img_suffix`` is fixed to '.png' and ``seg_map_suffix`` is fixed to
+    '.ah.png'.
+    """
+
+    METAINFO = dict(
+        classes=("point","background"),
+        palette=[[255,255,255],[0,0,0]])
+
+    def __init__(self,
+                 seg_map_suffix='.png',
+                 img_suffix=".jpg",
+                 data_prefix=dict(
+                     img_path="radar",
+                     seg_map_path="lidar"
+                 ),
+                 reduce_zero_label=False,
+                 **kwargs) -> None:
+        super().__init__(
+            seg_map_suffix=seg_map_suffix,
+            img_suffix=img_suffix,
+            data_prefix=data_prefix,
+            reduce_zero_label=reduce_zero_label,
+            **kwargs)
+        
