c0facf9d5bd403949aad85c0d8ef9744c7d62ae1
David Hunt <david.hunt@duke.edu>
2023-12-14 12:45:22 -0500

    updated configurations

--- configs/_base_/datasets/radcloud.py
+++ configs/_base_/datasets/radcloud.py
@@ -2,41 +2,21 @@
 dataset_type = 'RadcloudDataset' 
 train_data_root = '/data/david/CPSL_Ground/train'
 test_data_root = "/data/david/CPSL_Ground/test"
-# crop_size = (512, 512)
-# img_scale = (2048,1024)
 train_pipeline = [
     dict(type='LoadImageFromNP'),
     dict(type='LoadAnnotationsNP'),
-    # dict(
-    #     type='RandomResize',
-    #     scale=img_scale,
-    #     ratio_range=(0.5, 2.0),
-    #     keep_ratio=True),
-    # dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
-    # dict(type='RandomFlip', prob=0.5),
-    # dict(type='PhotoMetricDistortion'),
     dict(type='PackSegInputs')
 ]
 test_pipeline = [
     dict(type='LoadImageFromNP'),
-    # dict(type='Resize', scale=img_scale, keep_ratio=True),
     dict(type='LoadAnnotationsNP'),
     dict(type='PackSegInputs')
 ]
-# img_ratios = [0.5, 0.75, 1.0, 1.25, 1.5, 1.75]
 tta_pipeline = [
     dict(type='LoadImageFromNP', file_client_args=dict(backend='disk')),
     dict(
         type='TestTimeAug',
         transforms=[
-            # [
-            #     dict(type='Resize', scale_factor=r, keep_ratio=True)
-            #     for r in img_ratios
-            # ],
-            # [
-            #     dict(type='RandomFlip', prob=0., direction='horizontal'),
-            #     dict(type='RandomFlip', prob=1., direction='horizontal')
-            # ], 
             [
                 dict(type='LoadAnnotationsNP')
             ],
